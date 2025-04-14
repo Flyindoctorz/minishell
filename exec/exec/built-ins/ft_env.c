a606b0775ab871e8522c24ed37f8e2a4797b93e6
Louai Mokhtari <lmokhtar@bess-f2r4s7.clusters.42paris.fr>
2025-04-14 21:29:30 +0200

    env -i

--- exec/exec/built-ins/ft_env.c
+++ exec/exec/built-ins/ft_env.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmokhtar <lmokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 02:22:55 by lmokhtar          #+#    #+#             */
-/*   Updated: 2025/04/03 15:52:01 by lmokhtar         ###   ########.fr       */
+/*   Updated: 2025/04/14 21:22:46 by lmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,23 @@ static void	env_print(t_env *env)
 
 int	ft_env(t_data *minishell, char **arg)
 {
-	(void)arg;
+	int	i;
+
+	i = 1;
 	minishell->state = 0;
+	while (arg[i])
+	{
+		if (arg[i][0] == '-')
+		{
+			ft_putstr_fd("env: ", 2);
+			ft_putstr_fd(arg[i], 2);
+			ft_putstr_fd(": Invalid option\n", 2);
+			ft_putstr_fd("usage: env\n", 2);
+			minishell->state = 1;
+			return (minishell->state);
+		}
+		i++;
+	}
 	env_print(minishell->env);
 	return (minishell->state);
 }
